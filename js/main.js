042f0f2bc481229fa8320c49f524dfe9d6c93352
Abdulsalam <abdulsalammudasiru2021@gmail.com>
2022-12-13 07:40:20 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// show menu
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+})
+
